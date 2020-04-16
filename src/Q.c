18e1593e74b52599f22e64aaad4a7b300fe56297
TiagooGomess <tiagogomes2011@live.com.pt>
2020-04-16 19:37:58 +0100

    Added makefile

---
+++ src/Q.c
@@ -0,0 +1,9 @@
+#include "Q.h"
+
+int main(int argc, char* argv[]) {
+    
+    printf("hello from server\n");
+    
+    
+    exit(0);
+}
---
+++ src/U.c
@@ -0,0 +1,9 @@
+#include "U.h"
+
+int main(int argc, char* argv[]) {
+    
+    printf("hello from client\n");
+    
+    
+    exit(0);
+}
